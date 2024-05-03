fabd105903c2b552111fc2f00faf149c953acf12
Tabitha Harrington <tabithaharrington@Tabithas-Air.lan>
2024-05-02 20:58:42 -0600

    assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,40 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+    List<int> numbers = new List<int>();
+    int chosen = -1;
+    while (chosen != 0)
+        {
+        Console.Write("Enter a number (0 to quit): ");   
+        string userResponse = Console.ReadLine();
+        chosen = int.Parse(userResponse);
+        if (chosen != 0)
+            {
+            numbers.Add(chosen);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+            {
+            sum += number;
+            }
+
+        Console.WriteLine($"The sum is: {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+            max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
     }
 }
