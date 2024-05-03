21a3cb70f5d71580c53c5fb6ffd8cde68110b46f
Tabitha Harrington <tabithaharrington@Tabithas-Air.lan>
2024-05-02 18:35:40 -0600

    Final Answer

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,12 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        string response = "yes";
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
 
-        while (response == "yes")
+        int guess = -1;
+        
+        while (guess != magicNumber)
         {
-        Console.Write("Do you want to continue? ");
-        response = Console.ReadLine();
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
     }
 }
